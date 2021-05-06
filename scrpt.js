ee4f003e25a710cfe2c6dd2e7402b4199d12ad41
Siyabulela <siyak@Admin.local>
2021-05-06 13:18:14 +0200

    Made requested changes

--- scrpt.js
+++ scrpt.js
@@ -1,8 +1,8 @@
-let value_arr = [];
-let count = 0;
-let score = 0;
+let value_arr = [],
+  count = 0,
+  score = 0;
 
-let position = [
+const position = [
   'r1c1,r2c1,r3c1,r4c1,r5c1,r6c1,r7c1,r8c1,r9c1,r10c1,r11c1', // Competitive
   'r2c11,r3c11,r4c11,r5c11,r6c11,r7c11,r8c11,r9c11,r10c11', // Inclusive
   'r5c1,r5c2,r5c3,r5c4,r5c5,r5c6,r5c7,r5c8,r5c9,r5c10', // Excellence
@@ -12,19 +12,21 @@ let position = [
 
 window.onclick = (e) => {
   if (e.target.innerText.length == 1 && validCell(e.target.id, position)) {
-    let id = e.target.id;
-    let str_value = addOrRemoveValue(id, e);
-    let foundMatch = checkWord(str_value, position);
+    const id = e.target.id,
+      str_value = addOrRemoveValue(id, e),
+      foundMatch = checkWord(str_value, position);
+
     if (foundMatch[0]) {
-      let elementFound = document.getElementById(foundMatch[1]);
+      const elementFound = document.getElementById(foundMatch[1]);
       elementFound.innerHTML = `<strike>${elementFound.innerText}</strike>`;
+      removeWordFromPosition(foundMatch[1]);
       value_arr = [];
       count++;
       score += 400;
       document.getElementById('score').innerText = score;
     }
     if (count == 5) {
-      document.getElementById('score').innerText = 2000; //?
+      document.getElementById('score').innerText = score; //?
       document.getElementById('btnSubmit').disabled = false;
     }
   }
@@ -36,28 +38,37 @@ function onSubmit() {
 
 //CHECKS THE WORD IN THE WORD SEARCH
 function checkWord(values, arr_position) {
-  let string_found = false;
-  let index_found = 0;
+  let string_found = false,
+  index_found = 0,
+  sorted_value = '';
 
+  if (typeof values == 'object') {
+    sorted_value = values.sort();
+  }
   for (let r = 0; r < arr_position.length; r++) {
-    if (values == arr_position[r]) {
-      string_found = true;
-      index_found = r;
-      break;
+    let position_sort = arr_position[r].toString().split(',').sort();
+    if (sorted_value.length == position_sort.length) {
+      if (sorted_value.join('') == position_sort.join('')) {
+        string_found = true;
+        index_found = r;
+        break;
+      }
     }
   }
   //RETURNS BOOLEAN (FOUND) AND INDEX OF THE WORD IN THE ARRAY
   return [string_found, index_found];
 }
+//REMOVES WORD FROM POSITION ARRAY AFTER STRETCHED
+function removeWordFromPosition(r) {
+  position[r] = '';
+}
 
 function addOrRemoveValue(value, e) {
-  let found = false;
-  let item_index = 0;
-
+  let found = false,
+    item_index = 0;
   // CHECKING IF ARRAY IS EMPTY, PUSHES FIRST VALUE, CHANGES COLOR
   if (value_arr.length == 0) {
     value_arr.push(value);
-
     e.target.style.color = 'red';
     return value;
   }
@@ -67,6 +78,8 @@ function addOrRemoveValue(value, e) {
     if (value == value_arr[i]) {
       found = true;
       item_index = i;
+      value_arr.splice(item_index, 1);
+      e.target.style.color = 'black';
     }
   }
   //IF THE VALUE IS NOT IN THE ARRAY, IT'LL PUSH() IT
@@ -76,17 +89,14 @@ function addOrRemoveValue(value, e) {
     e.target.style.color = 'red';
   }
   //RETURN AN ARRAY AS A STRING
-  return value_arr.toString();
+  return value_arr;
 }
 
 // When a user clicks on a letter that is NOT in one of the target words, IGNORE
 function validCell(value, arr) {
   let arr_new = arr.toString();
-
-  console.log([value, arr_new]);
   if (arr_new.includes(value)) {
     return true;
   }
-
   return false;
 }
